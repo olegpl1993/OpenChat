7b63af704eec29404740e1298de5562fb7f09321
Oleg <olegpl1993@gmail.com>
2026-01-27 21:02:40 +0200

    refactor and styles

--- src/client/client.js
+++ src/client/client.js
@@ -1,10 +1,14 @@
-import { decrypt, encrypt } from "./crypt.js";
+import { encrypt } from "./crypt.js";
+import { addMessage } from "./render.js";
+import { setupLocalStorage } from "./saveLS.js";
 typeof process !== "undefined" && import("./styles.css");
 
 const nameInput = document.getElementById("nameInput");
 const chatInput = document.getElementById("chatInput");
 const keyInput = document.getElementById("keyInput");
 const button = document.getElementById("button");
+
+setupLocalStorage(nameInput, keyInput);
 
 const protocol = location.protocol === "https:" ? "wss" : "ws";
 const socket = new WebSocket(`${protocol}://${location.host}`);
@@ -42,36 +46,3 @@ async function sendMessage() {
 
   chatInput.value = "";
 }
-
-async function addMessage(name, text) {
-  const isUser = name === nameInput.value;
-  const decryptedText = await decrypt(text, keyInput.value);
-
-  const userName = document.createElement("p");
-  userName.className = `userName ${isUser ? "user" : "other"}`;
-  userName.textContent = `${name}`;
-
-  const messageText = document.createElement("p");
-  messageText.className = `messageText ${isUser ? "user" : "other"}`;
-  messageText.textContent = `${decryptedText}`;
-
-  const message = document.createElement("div");
-  message.className = `message ${isUser ? "user" : "other"}`;
-  message.appendChild(userName);
-  message.appendChild(messageText);
-
-  const messages = document.getElementById("messages");
-  messages.appendChild(message);
-  messages.lastElementChild?.scrollIntoView({ behavior: "smooth" });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  const savedName = localStorage.getItem("name");
-  const savedKey = localStorage.getItem("key");
-  if (savedKey) keyInput.value = savedKey;
-  if (savedName) nameInput.value = savedName;
-});
-window.addEventListener("beforeunload", () => {
-  localStorage.setItem("name", nameInput.value);
-  localStorage.setItem("key", keyInput.value);
-});
